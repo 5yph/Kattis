040f298993dff6290f469699498e29f1fa833c5a
5yph <lawrencewang2@gmail.com>
2020-12-21 16:28:33 -0700

    Solved

---
+++ 1.x/R2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int R1;
+    int S;
+    cin >> R1;
+    cin >> S;
+
+    int R2 = (2*S) - R1;
+    
+    cout << R2 << endl;
+
+	return 0;
+}
