ad79b4a125f650d74b8b06780a2eea6eed40d24a
5yph <lawrencewang2@gmail.com>
2020-12-21 20:46:49 -0700

    Solved

---
+++ 1.x/Last_Factorial.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int factorial(int x) // Calculates factorial using recursion
+{
+    if (x == 1) // Base case
+    {
+        return x;
+    }
+    else
+    {
+        return x * factorial(x - 1);
+    }
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    int fact = factorial(x);
+    // Convert fact to a character array for iterating
+    string factorial = to_string(fact);
+    cout << factorial.back() << endl;
+
+	return 0;
+}
