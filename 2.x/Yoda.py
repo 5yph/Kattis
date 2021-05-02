f4b902fb61b720e970cceb8d53dd8196ce279df1
5yph <lawrencewang2@gmail.com>
2021-05-02 17:25:22 -0600

    Solved

--- 2.x/Yoda.py
+++ 2.x/Yoda.py
@@ -1,3 +1,32 @@
+def remove_indices(indices, string):
+	""" removes from any string all chars
+	at specific indices """
+
+	offset = 0
+	# when we remove indices in order, the
+	# higher indices become 1 lower
+	for i in indices:
+		string = string[:i+offset] + string[i+1+offset:]
+		offset -= 1
+
+	return string
+
+
+def remove_leading_zeroes(string):
+	""" removes all leading zeroes from an integer-like string"""
+	leading_zeroes = True
+	index = 0
+	final_string = string
+
+	while(leading_zeroes and index < len(string)-1):
+		if string[index] == '0':
+			final_string = final_string.replace(string[index], '', 1)
+			index += 1
+		else:
+			leading_zeroes = False # no more leading zeroes, break
+
+	return final_string
+
 code1 = input()
 code2 = input()
 
@@ -21,41 +50,22 @@ elif code2_shorter:
 	for i in range(len(code1) - len(code2)):
 		code2 = '0' + code2
 
-output1 = code1
-output2 = code2
-
+code1_to_remove = []
+code2_to_remove = []
+# Record which indices need to be removed
 for i in range(length):
-	# all codes should be equal now
+	# all codes should be equal length
 	if int(code1[i]) < int(code2[i]):
-		output1 = output1.replace(code1[i], '', 1)
-		# remove 1 instance of that integer
+		code1_to_remove.append(i)
 	elif int(code2[i]) < int(code1[i]):
-		output2 = output2.replace(code2[i], '', 1)
+		code2_to_remove.append(i)
+
+output1 = remove_indices(code1_to_remove, code1)
+output2 = remove_indices(code2_to_remove, code2)
 
 # check for leading zeroes
-leading_zeroes = True
-index = 0
-length = len(output1)
-formatted_output1 = output1
-
-while(leading_zeroes and index < length-1):
-	if output1[index] == '0':
-		formatted_output1 = formatted_output1.replace(output1[index], '', 1)
-		index += 1
-	else:
-		leading_zeroes = False
-
-leading_zeroes = True
-index = 0
-length = len(output2)
-formatted_output2 = output2
-
-while(leading_zeroes and index < length-1):
-	if output2[index] == '0':
-		formatted_output2 = formatted_output2.replace(output1[index], '', 1)
-		index += 1
-	else:
-		leading_zeroes = False
+formatted_output1 = remove_leading_zeroes(output1)
+formatted_output2 = remove_leading_zeroes(output2)
 
 if len(formatted_output1) == 0:
 	print("YODA")
